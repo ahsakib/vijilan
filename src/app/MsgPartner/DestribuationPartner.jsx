f5a4c7afd1ff0f0e4b3146cfea9b572213853138
MD. Jahid Hasan <jh18186676@gmail.com>
2025-07-04 16:38:38 +0600

    distribution partner page design update

--- src/app/MsgPartner/DestribuationPartner.jsx
+++ src/app/MsgPartner/DestribuationPartner.jsx
@@ -46,41 +46,42 @@ const DestribuationPartner = ({ }) => {
 
 
     return <>
-        <div className="max-w-[90%] mx-auto py-16">
+        <div className="container mx-auto px-6 py-16">
             <section className="bg-transparent max-w-7xl mx-auto">
                 <div className="text-center text-white font-bold mb-12">
-                    <h1 className="text-3xl md:text-5xl mb-4">
-                        Procure Vijilan Through Your Preferred Distributor
+                    <h1 className="text-3xl md:text-[40px] font-bold mb-4">
+                        Procure Vijilan Through Your Preferred  <br /> Distributor
                     </h1>
-                    <p className="text-lg md:text-xl font-normal max-w-3xl mx-auto text-slate-200">
-                        We partner with the industry's leading technology distributors to make quoting, purchasing, and deploying our managed security solutions seamless and efficient for our enterprise clients and channel partners.
+                    <p className="text-lg md:text-[18px]  text-white">
+                        We partner with the industry's leading technology distributors to make quoting, purchasing, <br /> and deploying our managed security solutions seamless and efficient for our enterprise clients <br /> and channel partners.
                     </p>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-10 justify-items-center">
                     {distributors.map((icon, index) => (
                         <SimpleCard
                             key={index}
                             icon={icon}
                             h="148px"
                             w="362px"
+                            center={true}
                         />
                     ))}
                 </div>
             </section>
-            <section className="bg-transparent max-w-7xl mx-auto">
-                <h1 className="text-3xl md:text-5xl mb-4">
+            <section className="bg-transparent max-w-7xl mx-auto py-20">
+                <h1 className="text-3xl md:text-[40px] font-bold text-white text-center mb-4">
                     Streamlined Procurement, Simplified Billing
                 </h1>
-                <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-10 justify-items-center">
+                <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 justify-items-center gap-x-14">
                     {cardData.map((card, index) => (
-                        <CardBorder key={index} height="180px" width="512px">
-                            <div className="flex items-start gap-4 text-left text-white px-6 py-4">
-                                <div className="bg-[#00AEEF] rounded-[8px] p-2">
-                                    <img src={card.icon} alt="icon" className="w-5 h-5" />
+                        <CardBorder key={index} height="290px" width="564px">
+                            <div className="flex flex-col items-start gap-4 text-left text-white px-[32px] py-[40px]">
+                                <div className="bg-[#00AEEF] rounded-[8px] w-[64px] h-[64px] flex items-center justify-center">
+                                    <img src={card.icon} alt="icon" className="w-[40px] h-[40px]" />
                                 </div>
                                 <div>
-                                    <h2 className="text-base font-semibold mb-1">{card.title}</h2>
-                                    <p className="text-sm text-white/80 leading-snug">{card.desc}</p>
+                                    <h2 className="text-[24px] font-medium mb-1">{card.title}</h2>
+                                    <p className="text-[18px] text-white leading-snug">{card.desc}</p>
                                 </div>
                             </div>
                         </CardBorder>
@@ -89,11 +90,11 @@ const DestribuationPartner = ({ }) => {
             </section>
             <section className="bg-transparent max-w-7xl mx-auto">
                 <div className="text-center text-white font-bold mb-12">
-                    <h1 className="text-3xl md:text-5xl mb-4">
+                    <h1 className="text-3xl md:text-[40px] mb-4">
                         Our Esteemed Distribution Partners
                     </h1>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-10 justify-items-center">
                     {titles.map((title, index) => (
                         <SimpleCard
                             key={index}
@@ -104,9 +105,9 @@ const DestribuationPartner = ({ }) => {
                     ))}
                 </div>
             </section>
-            <section className="bg-transparent max-w-7xl mx-auto">
-                <div className="text-center text-white font-bold mb-12">
-                    <h1 className="text-3xl md:text-5xl mb-4">
+            <section className="bg-transparent max-w-7xl mx-auto py-20">
+                <div className="text-center text-white font-bold mb-6">
+                    <h1 className="text-3xl md:text-[40px]">
                         Getting Started is Easy
                     </h1>
 
@@ -114,42 +115,49 @@ const DestribuationPartner = ({ }) => {
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 justify-items-center">
                     <CardBorder width="567px" height="279px">
                         <div className="text-white p-4">
-                            <h1 className="font-bold text-2xl mb-4">Contact Your Distributor</h1>
-                            <p className="text-left font-bold">Reach out to your account manager at any of our listed distribution partners and ask for Vijilan's managed security services and solutions.</p>
+                            <h1 className="font-medium text-[24px] mb-4">Contact Your Distributor</h1>
+                            <p className="text-left text-[18px]">Reach out to your account manager at any of our listed <br /> distribution partners and ask for Vijilan's managed security <br /> services and solutions.</p>
                         </div>
                     </CardBorder>
                     <CardBorder width="567px" height="279px">
                         <div className="text-white p-4">
-                            <h1 className="font-bold text-2xl mb-4">Connect with Us</h1>
-                            <p className="text-left font-bold">Your distributor's representative will work directly with our channel team to scope your specific needs and generate a quote through their system.</p>
+                            <h1 className="font-medium text-[24px] mb-4">Connect with Us</h1>
+                            <p className="text-left text-[18px]">Your distributor's representative will work directly with our <br /> channel team to scope your specific needs and generate a <br /> quote through their system.</p>
                         </div>
                     </CardBorder>
                 </div>
             </section>
             <section className="bg-transparent max-w-7xl mx-auto">
 
-                <NavyBlueBackground>
+                <NavyBlueBackground borderCancel={true}>
                     <div>
                         <div className="grid grid-cols-2">
-                            <div className=' p-5 flex flex-col items-center justify-center border-[3px] rounded-2xl border-[#035882]'>
-                                <div className='-mb-10 flex flex-col items-center justify-center  gap-3'>
-                                    <div className='bg-gradient-to-t h-24 w-24  rounded-full flex justify-center items-center text-white text-lg font-bold from-[#07ADE9] to-[#EB9C33]'>
-                                        JN </div>
-                                    <h3 className='font-bold text-xl text-white'>JEN AI</h3>
-                                </div>
+                            <div className=' px-5 py-8 flex flex-col items-center justify-center border-[1px] rounded-2xl border-[#00AEEF3D]'>
+                                <div className="w-[61px] h-[61px] rounded-full flex items-center justify-center mx-auto mb-2"
+                                    style={{
+                                        background: `linear-gradient(0deg, rgba(196, 196, 196, 0.5), rgba(196, 196, 196, 0.5)),linear-gradient(180deg, #F89B29 0%, #00AEEF 100%)`,
+                                        backgroundBlendMode: "overlay"
+                                    }}>
 
-                                <Heading_Btn
-                                    desc='""Have a question about a specific distributor or how to add Vijilan to your next order? I can help guide you.""'
-                                    btn4="start a chat"
-                                />
-                            </div>
-                            <div class="max-w-2xl mx-auto p-8  rounded-lg shadow-sm text-center">
-                                <h1 class="text-2xl font-bold text-white mb-5">Don't See Your Distributor?</h1>
-                                <p class="text-white mb-6 leading-relaxed">
-                                    We're always expanding our distribution network.
-                                        Contact our channel team to discuss adding your preferred distributor or to learn more about our procurement options.
+                                    <h3 className="text-[20px] font-bold text-white ">JN</h3>
+                                </div>
+                                <h3 className="text-[20px] font-bold text-white mb-6">JEN AI</h3>
+
+                                <p className="text-white text-[16px] text-center mb-12">
+                                    "Have a question about a specific distributor or <br /> how to add Vijilan to your next order? I can help <br /> guide you."
                                 </p>
-                                <GradientButton children={"Schedule a compliance strategy call"}/>
+
+                                <div  className="border border-[#00AEEF] rounded-[6px]">
+                                    <button className="border border-[#00AEEF] font-medium rounded-[6px] text-white text-[13px] p-[16px]">ASK JEN AI</button>
+                                </div>
+                            </div>
+                            <div class="w-full p-8  rounded-lg ">
+                                <h1 class="text-[40px] font-bold text-white mb-5">Don't See Your Distributor?</h1>
+                                <p class="text-white text-[18px] text-center mb-6 leading-relaxed">
+                                    We're always expanding our distribution network. <br />
+                                    Contact our channel team to discuss adding your <br /> preferred distributor or to learn more about our <br /> procurement options.
+                                </p>
+                                <GradientButton children={"Schedule a compliance strategy call"} />
                             </div>
                         </div>
                     </div>
--- src/components/NavyBlueBackground.jsx
+++ src/components/NavyBlueBackground.jsx
@@ -1,11 +1,11 @@
 
-const NavyBlueBackground = ({ children }) => {
+const NavyBlueBackground = ({ children, borderCancel }) => {
     return <>
 
         <section className="py-20 px-4 ">
             <div className="container mx-auto">
                 <div
-                    className="backdrop-blur-md rounded-3xl p-12 max-w-5xl mx-auto shadow-2xl border border-[#00AEEF]/40"
+                    className={`backdrop-blur-md rounded-3xl p-12 max-w-5xl mx-auto  ${borderCancel ? "" : "shadow-2xl border border-[#00AEEF]/40"} `}
                     style={{
                         background: "linear-gradient(0deg, #082235 54%, #00AEEF 328%)",
                     }}
