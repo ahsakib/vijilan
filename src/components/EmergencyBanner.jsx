64c63faa541c5687e6291cde9ed8fc759754f9f1
MD. Jahid Hasan <jh18186676@gmail.com>
2025-07-07 10:13:07 +0600

    header responsive

--- src/components/EmergencyBanner.jsx
+++ src/components/EmergencyBanner.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 const EmergencyBanner = () => {
   return (
-    <div className="w-[1160px] mx-auto">
+    <div className="max-w-[1160px] mx-auto">
       <div
         className="flex justify-center items-center backdrop-blur-3xl h-[64px] rounded-[12px] px-6 gap-4"
         style={{
           backgroundImage: "linear-gradient(to right, #F89B29, #FF0F7B)",
         }}>
         <span className="text-white text-[18px]">Under Attack or Suspect a Breach?</span>
-        <button className="bg-white text-[13px] !text-[#082235] hover:bg-gray-100 rounded-[8px] font-medium py-[12px] px-[14px]">
+        <button className="bg-white text-[10px] sm:text-[13px] !text-[#082235] hover:bg-gray-100 rounded-[8px] font-medium py-[12px] sm:py-[12px] px-[8px] sm:px-[14px]">
           GET HELP NOW
         </button>
       </div>
